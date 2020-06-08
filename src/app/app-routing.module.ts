7bf87f7960fcbdc6c32f131f5826049c7a881c86
IrinaKhusid <66273115+IrinaKhusid@users.noreply.github.com>
2020-06-08 20:01:20 +0000

    end of monday

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoContainerComponent } from './components/todo-container/todo-container.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'todo',
+    component: TodoContainerComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
